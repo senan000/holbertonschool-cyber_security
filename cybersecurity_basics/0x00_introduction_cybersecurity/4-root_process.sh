80ab85a6082893499851e89d8ee78a140ba6aa80
senan000 <13737@holbertonstudents.com>
2026-04-27 18:50:24 +0000

    Fix task 4 filtering

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,command | grep -vE '\s+0\s+0\s+'
+ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,command | grep -vE "^[^ ]+ +[^ ]+ +[^ ]+ +[^ ]+ +0 +0 "
